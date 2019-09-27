c6a1c10f88167f6eb7ad27bd57bed45672b703ef
Grant Hobbs <grant@gwhobbs.com>
2019-09-27 17:27:52 -0500

    Update version of jquery-textcomplete to 1.8.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Add a Meteor.isMobile boolean variable",
-  version: "0.8.0_1",
+  version: "1.8.5",
   name: "mquandalle:jquery-textcomplete",
   git: "https://github.com/mquandalle/meteor-jquery-textcomplete.git"
 });
@@ -9,5 +9,5 @@ Package.onUse(function (api) {
   api.versionsFrom("METEOR@0.9.0");
   api.use("jquery", "client");
   var basePath = "jquery-textcomplete/dist/jquery.textcomplete";
-  api.addFiles([basePath + ".js", basePath + ".css"], "client");
+  api.addFiles([basePath + ".js"], "client");
 });
